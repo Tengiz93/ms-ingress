3a2336e170c9254cc62b23d91cfde010a6c0c2dd
ASUS <farzullasoy.tengiz@gmail.com>
2023-05-05 07:26:10 +0400

    Added new line

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import jdk.swing.interop.SwingInterOpUtils;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
         System.out.println("Hello Azerbaijan!");
         System.out.println("Hello Azerbaijan");
         System.out.println("Heo Turkey");
         System.out.println("hello Italy");
 
-
+        System.out.println("Heo Turkey");
+        System.out.println("hello Rome");
 
     }
     }
